0ef15012e89de81f2d220006f79d199bb9be5e46
bapboy18 <bapboy18@yahoo.com.vn>
2015-11-25 23:57:42 +0700

    remove comment line

--- xemayviet/config/routes.rb
+++ xemayviet/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
   root             "static_pages#home"
-  # get "about"   => "static_pages#about"
-  # get "signup"  => "users#new"
   resources :users
 end
