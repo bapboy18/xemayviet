a8e737b72fc61ca5c276a082c3a218a58f55f2c1
bapboy18 <bapboy18@yahoo.com.vn>
2015-11-25 23:49:04 +0700

    add gem devise and interface

--- xemayviet/config/routes.rb
+++ xemayviet/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: "users/sessions" }
   root             "static_pages#home"
-  get "about"   => "static_pages#about"
-  get "signup"  => "users#new"
+  # get "about"   => "static_pages#about"
+  # get "signup"  => "users#new"
   resources :users
 end
