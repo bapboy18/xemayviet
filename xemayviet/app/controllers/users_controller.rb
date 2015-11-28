71e236b11c24abb4d685aed3a401d771834b6311
bapboy18 <bapboy18@yahoo.com.vn>
2015-11-29 00:13:32 +0700

    fix bug sign up

--- xemayviet/app/controllers/users_controller.rb
+++ xemayviet/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :configure_permitted_parameters
+
   def show
     @user = User.find params[:id]
   end
--- xemayviet/db/schema.rb
+++ xemayviet/db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20151124164446) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "password_digest",        limit: 255
+    t.string   "name",                   limit: 255, default: "", null: false
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
