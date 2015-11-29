90539daf828d85fd9cd0b65acd007795214d7ec4
bapboy18 <bapboy18@yahoo.com.vn>
2015-11-29 23:44:20 +0700

    can not display map

--- xemayviet/config/routes.rb
+++ xemayviet/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
   devise_for :users, controllers: { sessions: "users/sessions" }
   root             "static_pages#home"
   resources :users
